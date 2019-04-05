39fb93c1156f3df33cc2d9817b6c9250a058d767
Greg <gheidorn@cars.com>
2019-04-05 07:50:19 -0500

    set test config to normal port and info logging

--- config/test.exs
+++ config/test.exs
@@ -3,11 +3,11 @@ use Mix.Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :risk, RiskWeb.Endpoint,
-  http: [port: 4002],
+  http: [port: 4000],
   server: false
 
 # Print only warnings and errors during test
-config :logger, level: :debug
+config :logger, level: :info
 
 # Configure your database
 config :risk, Risk.Repo,
